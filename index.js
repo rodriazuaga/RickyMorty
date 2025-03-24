aa03ad219c1ea9e45072aa5810935b2c76367741
Rodrigo Azuaga <rodrigoazuaga19@gmail.com>
2025-03-24 23:13:09 +0100

    agregando redis y docker

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import express from 'express'
 import axios from 'axios'
+import responseTime from 'response-time'
+import { createClient } from 'redis'
 
 const app = express()
 
-app.get('/personaje', async (req, res)=>{
+const client = createClient({
+  host: 'localhost',
+  port: 6379
+})
+
+app.use(responseTime())
+
+app.get('/personajes', async (req, res)=>{
+  const reply = await client.get('characters')
+  if(reply) return res.json(JSON.parse(reply))
   const {data} = await axios.get('https://rickandmortyapi.com/api/character')
+
+  const saveResult = await client.set('characters', JSON.stringify(data))
   return res.json(data)
 })
 
-app.listen(3000, () =>{
-  console.log('Sirviendo en puerto 3000')
-})
+const main = async () => {
+  await client.connect()
+  app.listen(3000)
+    console.log('Sirviendo en puerto 3000')
+}
+
+main()
+
+
